a44c0d51e00489d8f1f13880485fa751946d3034
WillHoughton <contact@willhoughton.me>
2019-10-11 16:56:14 +1300

    styled tech images

--- client/components/Technologies.jsx
+++ client/components/Technologies.jsx
@@ -3,24 +3,24 @@ import React, { Component, Fragment } from 'react'
 
 
 export default function Render() {
-  return  (
-      <div id='techWrapper'>
-          <p id='techHeader'>Technologies</p>
-          <div id='techImages'>
-              <img src="/assets/images/css.png" alt="image"  className='techImage'/>
-              <img src="/assets/images/html.png" alt="image" className='techImage'/>
-              <img src="/assets/images/javascript.png" alt="image" className='techImage'/>
-              <img src="/assets/images/handlebars.png" alt="image" className='techImage'/>
-              <img src="/assets/images/jest.png" alt="image" className='techImage'/>
-              <img src="/assets/images/node.png" alt="image" className='techImage'/>
-              <img src="/assets/images/postgreSQL.png" alt="image" className='techImage'/>
-              <img src="/assets/images/react.png" alt="image" className='techImage'/>
-              <img src="/assets/images/redux.png" alt="image" className='techImage'/>
-              <img src="/assets/images/react-native.png" alt="image" className='techImage'/>
-          </div>
-      </div>
-    
-  )
+    return (
+        <div id='techWrapper'>
+            <p id='techHeader'>Technologies</p>
+            <div id='techImages'>
+                <img src="/assets/images/css.png" alt="image" className='techImage' />
+                <img src="/assets/images/html.png" alt="image" className='techImage' />
+                <img src="/assets/images/javascript.png" alt="image" className='techImage' />
+                <img src="/assets/images/handlebars.png" alt="image" className='techImage' />
+                <img src="/assets/images/jest.png" alt="image" className='techImage' />
+                <img src="/assets/images/node.png" alt="image" className='techImage' />
+                <img src="/assets/images/postgreSQL.png" alt="image" className='techImage' />
+                <img src="/assets/images/react.png" alt="image" className='techImage' />
+                <img src="/assets/images/redux.png" alt="image" className='techImage' />
+                <img src="/assets/images/react-native.png" alt="image" className='techImage' />
+            </div>
+        </div>
+
+    )
 }
 
 
