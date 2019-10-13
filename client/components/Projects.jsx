86a4e4e07ec937075ff5d83fd2896e2ffb0f2660
WillHoughton <contact@willhoughton.me>
2019-10-14 12:40:16 +1300

    fix heroku startup

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -13,26 +13,47 @@ export default function Projects() {
                     <img className='projectImage' src='/assets/images/code.jpeg' />
                     <p className='projectName'>Project 1</p>
                     <p className='projectDesc'>Description</p>
+                    <div className='buttonswrapper'>
+                        <a href="#" target="_blank"><p className='liv'>View Live</p></a>
+                        <a href="#" target="_blank"><p className='for'>Fork</p></a>
+                    </div>
+
                 </div>
                 <div className='project'>
                     <img className='projectImage' src='/assets/images/code.jpeg' />
                     <p className='projectName'>Project 2</p>
                     <p className='projectDesc'>Description</p>
+                    <div className='buttonswrapper'>
+                        <a href="#" target="_blank"><p className='liv'>View Live</p></a>
+                        <a href="#" target="_blank"><p className='for'>Fork</p></a>
+                    </div>
                 </div>
                 <div className='project'>
                     <img className='projectImage' src='/assets/images/code.jpeg' />
                     <p className='projectName'>Project 3</p>
                     <p className='projectDesc'>Description</p>
+                    <div className='buttonswrapper'>
+                        <a href="#" target="_blank"><p className='liv'>View Live</p></a>
+                        <a href="#" target="_blank"><p className='for'>Fork</p></a>
+                    </div>
                 </div>
                 <div className='project'>
                     <img className='projectImage' src='/assets/images/code.jpeg' />
                     <p className='projectName'>Project 4</p>
                     <p className='projectDesc'>Description</p>
+                    <div className='buttonswrapper'>
+                        <a href="#" target="_blank"><p className='liv'>View Live</p></a>
+                        <a href="#" target="_blank"><p className='for'>Fork</p></a>
+                    </div>
                 </div>
                 <div className='project'>
                     <img className='projectImage' src='/assets/images/code.jpeg' />
                     <p className='projectName'>Project 5</p>
                     <p className='projectDesc'>Description</p>
+                    <div className='buttonswrapper'>
+                        <a href="#" target="_blank"><p className='liv'>View Live</p></a>
+                        <a href="#" target="_blank"><p className='for'>Fork</p></a>
+                    </div>
                 </div>
                 <div className='project'>
                     <img className='projectImage' src='/assets/images/code.jpeg' />
